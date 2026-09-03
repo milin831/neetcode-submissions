f2c47ea3179836fd2213089fcd6cb6acb3a74f87
milin ajay marathe <66173649+milin831@users.noreply.github.com>
2026-09-03 15:35:22 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0) q.push({{i,j},0});            
+            }
+        }
+
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int time=q.front().second;
+            q.pop();
+            vis[r][c]=1;
+            int dr[]={1,-1,0,0};
+            int dc[]={0,0,1,-1};
+            for(int k=0;k<4;k++){
+                int row=r+dr[k];
+                int col=c+dc[k];
+                if(row<0 || row>=n || col<0 || col>=m || grid[row][col]==-1 || grid[row][col]==0 || vis[row][col]) continue;
+                if(grid[row][col]==2147483647){
+                    grid[row][col]=time+1;
+                    q.push({{row,col},time+1});
+                }
+                else{
+                    if(grid[row][col]>time){
+                        grid[row][col]=time+1;
+                        q.push({{row,col},time+1});
+                    }
+                }
+            }
+        }
+    } 
+};
